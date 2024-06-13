cfc270c533fa3f3bdf009ce79c336e5c9c6fbd12
cameronstackhouse <stackhousecameron@gmail.com>
2024-06-13 19:34:52 +0100

    Fix evaluate.py

--- src/Sampling_based_Planning/rrt_2D/evaluate.py
+++ src/Sampling_based_Planning/rrt_2D/evaluate.py
@@ -1,5 +1,5 @@
 """
-contains the functions related to the evaluation and comparison 
+contains the functions related to the evaluation and comparison of algorithms
 """
 
 import json
@@ -14,24 +14,40 @@ sys.path.append(
 
 from rrt_2D.mb_guided_srrt_edge import MBGuidedSRrtEdge
 from rrt_2D.rrt_edge import RrtEdge
+from rrt_2D.rrt import Rrt
+from rrt_2D.rrt_star import RrtStar
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../Evaluation/")
 from load_map import create_custom_env
+from stats import Stats
 
 from glob import glob
 from pathlib import Path
 
 
 def comapare(algo_1, algo_2):
+    """
+    TODO
+    """
     pass
 
 
-def gen_report():
-    MAP_DIR = "src/Evaluation/Maps/2D/block_map_25"
+def gen_report(MAP_DIR):
+    """
+    TODO
+    """
+    START = (0, 0)
+    END = (0, 0)
     map_name_list = list(Path(MAP_DIR).glob("*.json"))
     NUM_MAPS = len(map_name_list)
 
     # TODO algorithms
+    algorithms = [
+        MBGuidedSRrtEdge(START, END, 0.05, 0.1),
+        RrtEdge(START, END, 0.05, 2000),
+        Rrt(START, END, 4, 0.05, 2000),
+        RrtStar(START, END, 4, 0.05, 5, 2000),
+    ]
 
     # TODO metrics
     avg_path_len = 0
@@ -41,7 +57,7 @@ def gen_report():
     # Load and evaluate each map
     for map in map_name_list:
         # TODO run multiple algorithms
-        alg = MBGuidedSRrtEdge((0, 0), (0, 0), 0.05, 0.1)
+        alg = MBGuidedSRrtEdge((0, 0), (0, 0), 0.05, 0.6)
 
         alg.change_env(map)
 
@@ -49,14 +65,12 @@ def gen_report():
         path = alg.planning()
         total_time = time.time() - start_time
 
-        print(map)
-
         if path:
             success += 1
             avg_path_len += alg.utils.path_cost(path)
             avg_time += total_time
 
-           # alg.plotting.animation(alg.vertex, path, f"Bounded Guided SRRT-Edge {map}", False)
+        # alg.plotting.animation(alg.vertex, path, f"Bounded Guided SRRT-Edge {map}", False)
 
     # TODO Gen report here
     # for algorithm in algorithms:
@@ -75,4 +89,4 @@ def main():
 
 
 if __name__ == "__main__":
-    gen_report()
+    gen_report("src/Evaluation/Maps/2D/block_map_25")
