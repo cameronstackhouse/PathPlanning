1893a74c8b87124c1c9b017dd0ec66f9894ea860
cameronstackhouse <stackhousecameron@gmail.com>
2024-06-20 11:21:22 +0100

    Add invalidate and revalidate methods

--- src/Sampling_based_Planning/rrt_2D/dynamic_guided_srrt_edge.py
+++ src/Sampling_based_Planning/rrt_2D/dynamic_guided_srrt_edge.py
@@ -9,28 +9,12 @@ sys.path.append(
 from rrt_2D.mb_guided_srrt_edge import MBGuidedSRrtEdge
 
 
-class Node:
-    def __init__(self, n):
-        self.x = n[0]
-        self.y = n[1]
-        self.parent = None
-        self.flag = "VALID"
-        self.partition = 1
-
-
-class Edge:
-    def __init__(self, n_p, n_c):
-        self.parent = n_p
-        self.child = n_c
-        self.flag = "VALID"
-
-
 class DynamicObj:
     def __init__(self) -> None:
         self.velocity = []
-        self.shape = []
+        self.size = []
         self.known = False
-
+        self.current_pos = []
 
 class DynamicGuidedSRrtEdge(MBGuidedSRrtEdge):
     def __init__(
@@ -46,6 +30,9 @@ class DynamicGuidedSRrtEdge(MBGuidedSRrtEdge):
         super().__init__(
             start, end, goal_sample_rate, global_time, mem, min_edge_length
         )
+        self.dynamic_objects = []
+        self.invalidated_nodes = set()
+        self.invalidated_edges = set()
 
     def run(self):
         # Find initial global path
@@ -70,15 +57,81 @@ class DynamicGuidedSRrtEdge(MBGuidedSRrtEdge):
                     current_pos = next_pos
         else:
             print("No path found")
-
-    def invalidate(self):
+    
+    def update_object_positions(self):
+        # TODO
         pass
 
+    def update_world_view(self):
+        # TODO
+        pass
+
+    def in_dynamic_obj(self, node, obj):
+        """
+        TODO
+        """
+        x, y = node.coords
+        x0, y0 = obj.current_pos
+        width, height = obj.size
+        return (x0 <= x < x0 + width) and (y0 <= y < y0 + height)
+
+    def invalidate(self, obj):
+        """
+        Invalidates nodes and edges which have been blocked by a dynamic object. 
+        """
+        # Check if nodes exist lie within the current object
+        for node in self.vertex:
+            x, y = node.coords
+
+            if self.in_dynamic_obj(node, obj):
+                self.invalidated_nodes.add(tuple(node))
+        
+        # Check if any part of an edge lies within the object
+        for edge in self.edges:
+            n1 = edge.node_1
+            n2 = edge.node_2
+
+            if self.utils.is_collision(n1, n2):
+                self.invalidated_edges.add(tuple(edge))
+
+    def revalidate(self):
+        """
+        Assesses invalidated nodes and edges to see if they are no longer invalidated by
+        dynamic objects.
+        """
+        # Check nodes
+        for node in self.invalidated_nodes:
+            # Check if a node is still inside one of the dynamic objects
+            blocked = False
+            for object in self.dynamic_objects:
+                if self.in_dynamic_obj(node, object):
+                    blocked = True
+                    break
+            
+            if not blocked:
+                self.invalidated_nodes.remove(tuple(node))
+
+        # Check edges
+        for edge in self.invalidated_edges:
+            n1 = edge.node_1
+            n2 = edge.node_2
+
+            if not self.utils.is_collision(n1, n2):
+                self.invalidated_edges.remove(tuple(edge))
+        
+
     def regrow(self):
+        # TODO
         pass
 
     def reconnect(self):
-        pass
+        # Reassess the validity of nodes
+        for node in self.invalidated_nodes:
+            pass
+
+        # Reassess the validity of edges
+        for edge in self.invalidated_edges:
+            pass
 
 
 if __name__ == "__main__":
